fdfb31c0c3d49b34a7e3e5814358d2dcb28af4fa
gin.tama <1137554811@qq.com>
2019-03-21 19:52:43 +0800

    a

---
+++ chatRobot/talk.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: talk.cpp
+	> Author:Gin.TaMa 
+	> Mail:1137554811@qq.com 
+	> Created Time: 2019年03月17日 星期日 10时02分41秒
+ ************************************************************************/
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    return 0;
+}
